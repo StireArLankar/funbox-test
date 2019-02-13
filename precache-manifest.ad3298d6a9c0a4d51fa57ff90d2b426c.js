da01eb0aa4b294593f5a8ed999530b478dc2f65f
StireArLankar <stire.ru@gmail.com>
2019-02-13 18:39:31 +0300

    Updates

---
+++ precache-manifest.ad3298d6a9c0a4d51fa57ff90d2b426c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5506e1b78db9f9b8c6c5ee1a3b9dcf78",
+    "url": "/funbox-test/static/media/cat.5506e1b7.png"
+  },
+  {
+    "revision": "6d7f27dbe0939fa61c00660d062600e5",
+    "url": "/funbox-test/static/media/Exo20-Thin.6d7f27db.woff2"
+  },
+  {
+    "revision": "07eac44b9dcb7c4aa4fcc5711b8168c4",
+    "url": "/funbox-test/static/media/Exo20-Thin.07eac44b.woff"
+  },
+  {
+    "revision": "54c999a3b5344787f2d5",
+    "url": "/funbox-test/static/js/runtime~main.54c999a3.js"
+  },
+  {
+    "revision": "ecc8683bf5fb58000421",
+    "url": "/funbox-test/static/js/main.ecc8683b.chunk.js"
+  },
+  {
+    "revision": "96c73a1b06c867b14492",
+    "url": "/funbox-test/static/js/2.96c73a1b.chunk.js"
+  },
+  {
+    "revision": "ecc8683bf5fb58000421",
+    "url": "/funbox-test/static/css/main.bc1dce96.chunk.css"
+  },
+  {
+    "revision": "b8cdd6084137ed9378ce530278ce200d",
+    "url": "/funbox-test/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/funbox-test/precache-manifest.ad3298d6a9c0a4d51fa57ff90d2b426c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/funbox-test/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
